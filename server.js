1ecb3cb58b8d0587cf88d341c2aa1a5da5c6af0f
luiggy macias <ferrinluiggy@gmail.com>
2021-12-14 10:19:19 -0500

    fist commit

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
+app.set("view engine", "pug"); //establece que motor de vista se va a usar, osea el que va a renderizar la pagina hmtl
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('Load your view here');
+app.route("/").get((req, res) => {
+  res.render(process.cwd() + "/views/pug"); //uitiliza render usango el motor (engine) establecido (app.set)
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
