10716b47224bdf724c57635f6cad88a039a40583
luiggy macias <ferrinluiggy@gmail.com>
2021-12-27 14:41:36 -0500

    djdj

--- auth.js
+++ auth.js
@@ -1,1 +1,43 @@
-module.exports = function (app, myDataBase) {};
+const passport = require("passport");
+
+module.exports = function (app, myDataBase) {
+  // Serialization and deserialization here...
+
+  passport.serializeUser((user, done) => {
+    // serializa el usuario
+    done(null, user._id); // guarda el id del usuario
+  });
+  passport.deserializeUser((id, done) => {
+    // deserializa el usuario
+    myDataBase.findOne({ _id: new ObjectId(id) }, (err, doc) => {
+      // busca el usuario en la base de datos
+      done(null, doc); // guarda el usuario
+    });
+  });
+  passport.use(
+    // usa la estrategia local
+    new LocalStrategy(function (username, password, done) {
+      // estrategia local
+      myDataBase.findOne({ username: username }, function (err, user) {
+        // busca el usuario en la base de datos
+        console.log("User " + username + " attempted to log in."); // imprime en consola que el usuario intento loguearse
+        if (err) {
+          // si hay un error
+          return done(err); // retorna el error
+        }
+        if (!user) {
+          // si no encuentra el usuario
+          return done(null, false); // retorna null y false
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          // si la contraseña no es correcta
+          // si la contraseña no es igual a la contraseña del usuario
+          return done(null, false); // retorna null y false
+        }
+        // si todo sale bien retorna el usuario
+        return done(null, user);
+      });
+    })
+  );
+  // Be sure to add this...
+};
--- routes.js
+++ routes.js
@@ -1,1 +1,66 @@
-module.exports = function (app, myDataBase) {};
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+
+module.exports = function (app, myDataBase) {
+  // Be sure to change the title
+  app.route("/").get((req, res) => {
+    //Change the response to render the Pug template
+    res.render(process.cwd() + "/views/pug", {
+      title: "Connected to Database",
+      message: "Please login",
+      showLogin: true,
+      showRegistration: true,
+    });
+  });
+
+  app.route("/login").post(
+    passport.authenticate("local", { failureRedirect: "/" }), // la autenticacion es local, y si falla redirige a /
+    (req, res) => {
+      res.redirect("/profile"); //si la autenticacion es exitosa redirige a /profile
+    }
+  );
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect("/");
+  }
+  app.route("/profile").get(ensureAuthenticated, (req, res) => {
+    // si la autenticacion es exitosa redirige a /profile usando next() dentro de la funcion ensureAuthenticated, si no redirige a /
+    // res.render(process.cwd() + "/views/pug/index.pug", { showLogin: false });
+    res.render(process.cwd() + "/views/pug/profile.pug", {
+      username: req.user.username,
+    }); // renderiza el template profile.pug
+  });
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+  app.route("/register").post(
+    (req, res, next) => {
+      const hash = bcrypt.hashSync(req.body.password, 12); // encripta la contraseña
+      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect("/");
+        } else {
+          myDataBase.insertOne(
+            { username: req.body.username, password: hash },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile");
+    }
+  );
+};
