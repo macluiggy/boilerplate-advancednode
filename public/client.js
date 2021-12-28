296c8ec86bd282ab50ed393a8ac075b0231285ec
luiggy macias <ferrinluiggy@gmail.com>
2021-12-28 11:16:50 -0500

    updated

--- public/client.js
+++ public/client.js
@@ -2,12 +2,21 @@ $(document).ready(function () {
   /* Global io */
   let socket = io();
 
-  socket.on("user count", function (data) {
+  socket.on("user", function (data) {
     console.log(data);
+    $("#num-users").text(data.currentUsers + " users online");
+    let message =
+      data.name +
+      (data.connected ? " has joined the chat." : " has left the chat.");
+    $("#messages").append($("<li>").html("<b>" + message + "</b>"));
   });
-  socket.on("disconnect", () => {
-    /*anything you want to do on disconnect*/
+  socket.on("chat message", (data) => {
+    console.log("socket.on 1");
+    $("#messages").append($("<li>").text(`${data.name}: ${data.message}`));
   });
+  // socket.on("disconnect", () => {
+  //   /*anything you want to do on disconnect*/
+  // });
   // Form submittion with new message in field with id 'm'
   $("form").submit(function () {
     let messageToSend = $("#m").val();
--- server.js
+++ server.js
@@ -60,123 +60,26 @@ myDB(async (client) => {
   let currentUsers = 0;
   io.on("connection", (socket) => {
     ++currentUsers;
-    io.emit("user count", currentUsers);
+    io.emit("user", {
+      name: socket.request.user.name,
+      currentUsers,
+      connected: true,
+    });
+    socket.on("chat message", (message) => {
+      io.emit("chat message", { name: socket.request.user.name, message });
+    });
     console.log("A user has connected");
 
     socket.on("disconnect", () => {
       console.log("A user has disconnected");
       --currentUsers;
-      io.emit("user count", currentUsers);
+      io.emit("user", {
+        name: socket.request.user.name,
+        currentUsers,
+        connected: false,
+      });
     });
   });
-  // io.on("disconnect", () => {
-  //   --currentUsers;
-  //   io.emit("user count", currentUsers);
-  //   console.log("A user has disconnected");
-  // });
-  // // Be sure to change the title
-  // app.route("/").get((req, res) => {
-  //   //Change the response to render the Pug template
-  //   res.render(process.cwd() + "/views/pug", {
-  //     title: "Connected to Database",
-  //     message: "Please login",
-  //     showLogin: true,
-  //     showRegistration: true,
-  //   });
-  // });
-
-  // app.route("/login").post(
-  //   passport.authenticate("local", { failureRedirect: "/" }), // la autenticacion es local, y si falla redirige a /
-  //   (req, res) => {
-  //     res.redirect("/profile"); //si la autenticacion es exitosa redirige a /profile
-  //   }
-  // );
-  // function ensureAuthenticated(req, res, next) {
-  //   if (req.isAuthenticated()) {
-  //     return next();
-  //   }
-  //   res.redirect("/");
-  // }
-  // app.route("/profile").get(ensureAuthenticated, (req, res) => {
-  //   // si la autenticacion es exitosa redirige a /profile usando next() dentro de la funcion ensureAuthenticated, si no redirige a /
-  //   // res.render(process.cwd() + "/views/pug/index.pug", { showLogin: false });
-  //   res.render(process.cwd() + "/views/pug/profile.pug", {
-  //     username: req.user.username,
-  //   }); // renderiza el template profile.pug
-  // });
-  // app.route("/logout").get((req, res) => {
-  //   req.logout();
-  //   res.redirect("/");
-  // });
-  // app.route("/register").post(
-  //   (req, res, next) => {
-  //     const hash = bcrypt.hashSync(req.body.password, 12); // encripta la contraseña
-  //     myDataBase.findOne({ username: req.body.username }, function (err, user) {
-  //       if (err) {
-  //         next(err);
-  //       } else if (user) {
-  //         res.redirect("/");
-  //       } else {
-  //         myDataBase.insertOne(
-  //           { username: req.body.username, password: hash },
-  //           (err, doc) => {
-  //             if (err) {
-  //               res.redirect("/");
-  //             } else {
-  //               next(null, doc.ops[0]);
-  //             }
-  //           }
-  //         );
-  //       }
-  //     });
-  //   },
-  //   passport.authenticate("local", { failureRedirect: "/" }),
-  //   (req, res, next) => {
-  //     res.redirect("/profile");
-  //   }
-  // );
-  // app.use((req, res, next) => {
-  //   res.status(404).type("text").send("Not Found");
-  // });
-  // // Serialization and deserialization here...
-
-  // passport.serializeUser((user, done) => {
-  //   // serializa el usuario
-  //   done(null, user._id); // guarda el id del usuario
-  // });
-  // passport.deserializeUser((id, done) => {
-  //   // deserializa el usuario
-  //   myDataBase.findOne({ _id: new ObjectId(id) }, (err, doc) => {
-  //     // busca el usuario en la base de datos
-  //     done(null, doc); // guarda el usuario
-  //   });
-  // });
-  // passport.use(
-  //   // usa la estrategia local
-  //   new LocalStrategy(function (username, password, done) {
-  //     // estrategia local
-  //     myDataBase.findOne({ username: username }, function (err, user) {
-  //       // busca el usuario en la base de datos
-  //       console.log("User " + username + " attempted to log in."); // imprime en consola que el usuario intento loguearse
-  //       if (err) {
-  //         // si hay un error
-  //         return done(err); // retorna el error
-  //       }
-  //       if (!user) {
-  //         // si no encuentra el usuario
-  //         return done(null, false); // retorna null y false
-  //       }
-  //       if (!bcrypt.compareSync(password, user.password)) {
-  //         // si la contraseña no es correcta
-  //         // si la contraseña no es igual a la contraseña del usuario
-  //         return done(null, false); // retorna null y false
-  //       }
-  //       // si todo sale bien retorna el usuario
-  //       return done(null, user);
-  //     });
-  //   })
-  // );
-  // // Be sure to add this...
 }).catch((e) => {
   app.route("/").get((req, res) => {
     res.render("pug", { title: e, message: "Unable to login" });
